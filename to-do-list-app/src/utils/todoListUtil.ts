5501110787cc233c0daae7fec59ba1aec1f5dbf4
Victor Nieto <78815336+victorn650@users.noreply.github.com>
2026-09-20 19:25:24 -0700

    fixed how we set todos in App.tsx file

--- to-do-list-app/src/utils/todoListUtil.ts
+++ to-do-list-app/src/utils/todoListUtil.ts
@@ -2,6 +2,5 @@ export const getTodoList = async () => {
     const { VITE_BACKEND_SERVICE_URL } = import.meta.env;
     const result = await fetch(`${VITE_BACKEND_SERVICE_URL}/api/todos`);
     const todos = await result.json();
-    console.log('get to do list result:', todos);
     return todos;
 };
